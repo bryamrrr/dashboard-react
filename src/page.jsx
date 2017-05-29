f54ac1abfe520be70f2244116d73c9a5458f5406
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-05-29 12:46:36 -0500

    Add reset view

--- src/page.jsx
+++ src/page.jsx
@@ -6,21 +6,17 @@ import {
 
 import Login from './modules/login/view';
 import Signup from './modules/signup/view';
+import Reset from './modules/reset/view';
+import Retrieve from './modules/retrieve/view';
 
 export default function () {
   return (
     <main role="application">
       <Switch>
-        <Route
-          path="/login"
-          exact
-          component={Login}
-        />
-        <Route
-          path="/registro"
-          exact
-          component={Signup}
-        />
+        <Route path="/login" exact component={Login} />
+        <Route path="/registro" exact component={Signup} />
+        <Route path="/reset" exact component={Reset} />
+        <Route path="/users/:email/forgotpassword/:hash" exact component={Retrieve} />
       </Switch>
     </main>
   );
