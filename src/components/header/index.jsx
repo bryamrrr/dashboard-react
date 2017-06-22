ea16cd2a942127e12ff6b8951f87f74a0d7d1a18
Katherine De La Cruz <kdelacruz@rcp.pe>
2017-06-22 10:01:02 -0500

    Fix menu aside

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
 
 import BreadCrumb from '../../components/bread-crumb';
 import Settings from '../../modules/dashboard/settings';
@@ -6,9 +9,15 @@ import CartResume from '../../modules/dashboard/cart-resume';
 
 import styles from './styles.css';
 
-function Header() {
+function Header(props) {
   return (
     <header className={styles.header}>
+      <div className={styles.logo}>
+        <img
+          src={`${props.context}/images/logo.png`}
+          alt=""
+        />
+      </div>
       <i className={`${styles.icon} linearicon-list`} />
       <BreadCrumb />
       <Settings />
@@ -17,4 +26,12 @@ function Header() {
   );
 }
 
-export default Header;
+Header.propTypes = {
+  context: PropTypes.string.isRequired,
+};
+
+function mapStateToProps({ context }) {
+  return { context };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/components/menu-aside/index.jsx
+++ src/components/menu-aside/index.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import { connect } from 'react-redux';
 
 import MenuItem from '../menu-item';
 import routes from '../../extra/routes';
 
 import styles from './styles.css';
 
-function MenuAside(props) {
+function MenuAside() {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <img
-          src={`${props.context}/images/logo.png`}
-          alt=""
-        />
-      </header>
       <div className={styles.items}>
         {routes
           .map(menu => (
@@ -33,12 +24,4 @@ function MenuAside(props) {
   );
 }
 
-function mapStateToProps({ context }) {
-  return { context };
-}
-
-MenuAside.propTypes = {
-  context: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(MenuAside);
+export default MenuAside;
