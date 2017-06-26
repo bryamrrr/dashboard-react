3b7ca7022d12e20ae3a824cc5515a5fe6e436437
Katherine De La Cruz <kdelacruz@rcp.pe>
2017-06-26 10:22:20 -0500

    Add search bar

--- src/modules/user/contact/view/index.jsx
+++ src/modules/user/contact/view/index.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 
 import UserContactTable from '../user-contact-table';
 
+import Hexagon from '../../../../components/hexagon';
+import FormSearch from '../../../../components/form-search';
+import FormButton from '../../../../components/form-button';
+
 import styles from './styles.css';
 
 function UserContact() {
   return (
     <div>
-      <div className={styles.form}>
-        <UserContactTable />
+      <div className={styles.title}>
+        <Hexagon color="orange">
+          <i className="linearicon-users2" />
+        </Hexagon>
+        <h2>Mis contactos</h2>
       </div>
+      <div className={styles.filterContainer}>
+        <div className={styles.searchContainer}>
+          <FormSearch />
+        </div>
+        <div className={styles.buttonContainer}>
+          <FormButton
+            callToAction="Nuevo contacto"
+            includeIcon="linearicon-user"
+          />
+        </div>
+      </div>
+      <UserContactTable />
     </div>
   );
 }
