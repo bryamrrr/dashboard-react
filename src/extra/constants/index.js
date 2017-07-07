da3cb0f912b19fae21ce4552da98d703766fd1bf
Bryam Rodriguez <brodriguezayabr@gmail.com>
2017-07-07 16:30:48 -0500

    Wip hosting

--- src/extra/constants/index.js
+++ src/extra/constants/index.js
@@ -2,5 +2,6 @@ export default {
   urls: {
     API_SECURITY: 'http://staging-apisecurity.rcp.pe/api/v1',
     API_MOCKS: 'http://staging-backoffice.rcp.pe:8100/dashboard/api/v1',
+    MOCKABLE: 'https://demo2882768.mockable.io',
   },
 };
--- src/reducers/hostings/reducer.test.js
+++ src/reducers/hostings/reducer.test.js
@@ -1,6 +1,9 @@
 import reducer, { initialState } from './reducer';
 
-import { SET_HOSTINGS } from './actions';
+import {
+  SET_HOSTINGS,
+  SET_HOSTING_PRICES,
+} from './actions';
 
 describe('Domains - Reducer', () => {
   let action;
@@ -21,8 +24,21 @@ describe('Domains - Reducer', () => {
     expect(reducer(undefined, action)).toEqual(initialState);
   });
 
-  test('sets prices with the correct payload', () => {
+  test('sets hostings with the correct payload', () => {
     const newState = reducer(undefined, action);
+    expect(newState.products.get('aeb662ef-0117-450a-91e2-a067893e9767')).toEqual({
+      period: "Anual",
+      price: 178,
+      id: "aeb662ef-0117-450a-91e2-a067893e9767",
+    });
+  });
+
+  test('sets hostings prices with the correct payload', () => {
+    const newState = reducer(undefined, action);
+
+    const newAction = {
+      type
+    }
     expect(newState.products.get('aeb662ef-0117-450a-91e2-a067893e9767')).toEqual({
       period: "Anual",
       price: 178,
