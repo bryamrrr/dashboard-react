254841c09498c9bb72853681b448c05722298196
Katherine De La Cruz <kdelacruz@rcp.pe>
2017-07-19 12:40:19 -0500

    Validate user data form

--- src/modules/user/data/user-data-form/index.jsx
+++ src/modules/user/data/user-data-form/index.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form/immutable';
 
 import FormInput from '../../../../components/form-input';
 import Combo from '../../../../components/combo';
 import FormButton from '../../../../components/form-button';
 
 import { setLanguage } from '../../../../reducers/translate/actions';
+
+import regex from '../../../../regex';
 
 import styles from './styles.css';
 
@@ -16,11 +19,75 @@ class UserDataForm extends Component {
     super(props);
 
     this.changeLanguage = this.changeLanguage.bind(this);
+    this.renderUserField = this.renderUserField.bind(this);
+    this.renderEmailField = this.renderEmailField.bind(this);
+    this.renderNameField = this.renderNameField.bind(this);
+    this.renderDocumentField = this.renderDocumentField.bind(this);
+    this.renderPhoneField = this.renderPhoneField.bind(this);
+  }
+
+  onSubmit(values) {
+    console.log(values);
   }
 
   changeLanguage(data) {
     const language = (data.id === 1) ? 'es' : 'en';
     this.props.setLanguage(language);
+  }
+
+  renderUserField(field) {
+    return (
+      <FormInput
+        field={field}
+        name="user"
+        includeIcon="linearicon-user"
+        placeholder={this.props.strings.forms.user}
+      />
+    );
+  }
+
+  renderEmailField(field) {
+    return (
+      <FormInput
+        field={field}
+        name="email"
+        includeIcon="linearicon-envelope"
+        placeholder={this.props.strings.forms.email}
+      />
+    );
+  }
+
+  renderNameField(field) {
+    return (
+      <FormInput
+        field={field}
+        name="name"
+        includeIcon="linearicon-edit"
+        placeholder={this.props.strings.forms.name}
+      />
+    );
+  }
+
+  renderDocumentField(field) {
+    return (
+      <FormInput
+        field={field}
+        name="document"
+        includeIcon="linearicon-profile"
+        placeholder={this.props.strings.forms.document}
+      />
+    );
+  }
+
+  renderPhoneField(field) {
+    return (
+      <FormInput
+        field={field}
+        name="phone"
+        includeIcon="linearicon-phone"
+        placeholder={this.props.strings.forms.phone}
+      />
+    );
   }
 
   render() {
@@ -92,87 +159,123 @@ class UserDataForm extends Component {
     };
 
     return (
-      <div className={styles.container}>
-        <article>
-          <FormInput
-            name="user"
-            includeIcon="linearicon-user"
-            placeholder={this.props.strings.forms.user}
+      <div>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className={styles.container}>
+          <article>
+            <Field
+              name="user"
+              component={this.renderUserField}
+            />
+          </article>
+          <article>
+            <Field
+              name="email"
+              component={this.renderEmailField}
+            />
+          </article>
+          <article>
+            <Field
+              name="name"
+              component={this.renderNameField}
+            />
+          </article>
+          <article>
+            <FormInput
+              name="lastname"
+              includeIcon="linearicon-edit"
+              placeholder={this.props.strings.forms.lastname}
+            />
+          </article>
+          <article>
+            <Combo
+              includeIcon="linearicon-earth"
+              placeholder={this.props.strings.forms.country}
+              options={countries}
+            />
+          </article>
+          <article>
+            <Field
+              name="phone"
+              component={this.renderPhoneField}
+            />
+          </article>
+          <article>
+            <Combo
+              includeIcon="linearicon-register"
+              placeholder={this.props.strings.forms.documentType}
+              options={documentTypes}
+            />
+          </article>
+          <article>
+            <Field
+              name="document"
+              component={this.renderDocumentField}
+            />
+          </article>
+          <article>
+            <Combo
+              includeIcon="linearicon-users"
+              placeholder={this.props.strings.forms.personType}
+              options={customerTypes}
+            />
+          </article>
+          <article>
+            <Combo
+              includeIcon="linearicon-library"
+              placeholder={this.props.strings.forms.bussinessarea}
+              options={businessareas}
+            />
+          </article>
+          <article>
+            <Combo
+              includeIcon="linearicon-earth"
+              placeholder={this.props.strings.forms.language}
+              options={languages}
+              selected={languages['1']}
+              changeSelected={this.changeLanguage}
+            />
+          </article>
+          <FormButton
+            callToAction={this.props.strings.userData.title}
+            type="submit"
           />
-        </article>
-        <article>
-          <FormInput
-            name="email"
-            includeIcon="linearicon-envelope"
-            placeholder={this.props.strings.forms.email}
-          />
-        </article>
-        <article>
-          <Combo
-            includeIcon="linearicon-earth"
-            placeholder={this.props.strings.forms.country}
-            options={countries}
-          />
-        </article>
-        <article>
-          <FormInput
-            name="phone"
-            includeIcon="linearicon-phone"
-            placeholder={this.props.strings.forms.phone}
-          />
-        </article>
-        <article>
-          <Combo
-            includeIcon="linearicon-register"
-            placeholder={this.props.strings.forms.documentType}
-            options={documentTypes}
-          />
-        </article>
-        <article>
-          <FormInput
-            name="document"
-            includeIcon="linearicon-profile"
-            placeholder={this.props.strings.forms.document}
-          />
-        </article>
-        <article>
-          <Combo
-            includeIcon="linearicon-users"
-            placeholder={this.props.strings.forms.personType}
-            options={customerTypes}
-          />
-        </article>
-        <article>
-          <Combo
-            includeIcon="linearicon-library"
-            placeholder={this.props.strings.forms.field}
-            options={businessareas}
-          />
-        </article>
-        <article>
-          <Combo
-            includeIcon="linearicon-earth"
-            placeholder={this.props.strings.forms.language}
-            options={languages}
-            selected={languages['1']}
-            changeSelected={this.changeLanguage}
-          />
-        </article>
-        <FormButton
-          callToAction={this.props.strings.userData.title}
-        />
+        </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.get('user')) errors.user = 'Ingresa un usuario';
+  if (!values.get('email')) errors.email = 'Ingresa un email';
+
+  if (!values.get('email')) {
+    errors.email = 'Ingresa tu correo';
+  } else if (!regex.validate.email.test(values.get('email'))) {
+    errors.email = regex.message.email;
+  }
+
+  if (!values.get('name')) errors.name = 'Ingresa un nombre';
+  if (!values.get('document')) errors.document = 'Ingresa un número de documento';
+  if (!values.get('phone')) errors.phone = 'Ingresa un número de teléfono';
+
+  return errors;
+}
+
 UserDataForm.propTypes = {
   setLanguage: PropTypes.func.isRequired,
   strings: PropTypes.objectOf(PropTypes.object).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return { strings: state.get('translate').strings };
 }
 
-export default connect(mapStateToProps, { setLanguage })(UserDataForm);
+const UserDataReduxForm = reduxForm({
+  form: 'UserDataForm',
+  validate,
+})(UserDataForm);
+export default connect(mapStateToProps, { setLanguage })(UserDataReduxForm);
